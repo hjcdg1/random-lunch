d42a0e31b71a45a1b83ee2e03517aa02d94b86ee
hjcdg1 <hjcdg197@gmail.com>
2026-01-11 23:07:45 +0900

    [Improve] 빌드 타겟 단일화 (app 파일)

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,4 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
@@ -18,12 +14,7 @@ const config: ForgeConfig = {
     appBundleId: 'com.daangn.random-lunch',
   },
   rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
+  makers: [],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
